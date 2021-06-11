6b55775a44e992fa889dcb8ed0ca786f2ed5893c
Wajahat Muhammad <47386203+wajahatm@users.noreply.github.com>
2021-06-11 18:34:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.write("<h2 style='color: blue;'>JavaScript Rules!</h2>");
+document.write("<hr>");
+//alert("<hr>");
+
+var phrase = "Wajahat";
+document.write(phrase);
+phrase = "Muhammad";
+document.write(phrase);
+
+//Variable DataTypes
+var age = 26;
+var gpa = 3.1;
+var isMale = false;
+var flaws = null;
+var description = undefined;
