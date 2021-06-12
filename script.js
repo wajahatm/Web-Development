3f19dfcc3c907a7193b66749812928d94b6baa28
Wajahat Muhammad <47386203+wajahatm@users.noreply.github.com>
2021-06-12 12:05:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ var gpa = 3.1;
 var isMale = false;
 var flaws = null;
 var description = undefined;
+
+//Strings
+
+var str = "This is Temporary String";
+document.write("<hr>"+str+"<br>");
+document.write(str.length);
+document.write("<br>"+str.toUpperCase());
+document.write("<br>"+str.toLowerCase());
+document.write("<br>"+str.charAt(2));
+document.write("<br>"+str.indexOf("T"));
+document.write("<br>"+str.lastIndexOf("T"));
+document.write("<br>"+str.substring(0,5));
+document.write("<br>"+str.substring(str.indexOf("e"),str.length));
+document.write("<br>"+str.endsWith("String"));
+document.write("<br>"+str.includes("String"));
+
+//Math & Numbers
+
+document.write("<br>");
+document.write(2 + (4 * 8));
+var num = -9;
+document.write("<br>"+Math.abs(num));
+document.write("<br>"+Math.max(num,4));
+document.write("<br>"+Math.min(num,6));
+document.write("<br>"+Math.round(6.7));
+document.write("<br>"+Math.pow(3,3));
+document.write("<br>"+Math.sqrt(33));
+document.write("<br>"+Math.round(Math.random()* 10));
